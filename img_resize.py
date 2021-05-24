72e2f89e64bdd34fb2ad2a0a2dbd4eeb59e3d3eb
blesoft <ethic.fall.prg@gmail.com>
2021-05-24 18:25:25 +0900

    img_change

--- img_resize.py
+++ img_resize.py
@@ -12,7 +12,7 @@ width_origin  = img_origin.shape[1]
 comp_rate_array = [2,3,4,5,6,8,10,12,15,20]  #圧縮率
 for i in comp_rate_array:
     height_rewrite = int(height_origin / i)
-    width_rewrite  = int(height_origin / i)
+    width_rewrite  = int(width_origin / i)
     #  リサイズ
     img_rewrite = cv.resize(img_origin,(height_rewrite,width_rewrite))
     #  保存
